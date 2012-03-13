084ccc79a2a162d2d1908a71fcbff7d0c0765d45
Javier Arce <javierarce@gmail.com>
2012-03-13 20:01:34 +0100

    The filter closes when the user clicks outside it

--- js/app.js
+++ js/app.js
@@ -10,18 +10,31 @@ function initialize() {
 
 $(function() {
 
-    $("nav a.toggle-filter").on("click", function() {
+    $(document).on("click", function() {
+        if ($("nav a.toggle-filter").hasClass('selected')){
+            $("nav a.toggle-filter").removeClass('selected');
+            $(".filter").fadeOut(150);
+        }
+    });
+
+    $("nav a.toggle-filter").on("click", function(e) {
+        e.stopPropagation();
         $(this).toggleClass("selected");
         $(".filter").fadeToggle(150);
     });
 
-    $(".filter ul.ticks li").on("click", function() {
+    $(".filter ul.ticks li").on("click", function(e) {
+        e.stopPropagation();
         $(this).toggleClass("selected");
     });
 
-    $(".filter ul.radio li").on("click", function() {
+    $(".filter ul.radio li").on("click", function(e) {
+        e.stopPropagation();
         $(this).parent().find("li").removeClass("selected");
         $(this).addClass("selected");
     });
 
+    $(".filter").on("click", function(e) {
+        e.stopPropagation();
+    });
 });
