51a670d4806f061a2aac835b42c0573fc6005e3b
Javier Arce <javierarce@gmail.com>
2012-03-30 17:30:42 +0200

    Fixes relative paths

--- config.rb
+++ config.rb
@@ -13,7 +13,7 @@ preferred_syntax = :scss
 output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
