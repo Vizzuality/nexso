a4ac7907d1dda88c186adb8e1fee93606e717e1a
Javier Arce <javierarce@gmail.com>
2012-03-30 17:22:09 +0200

    Fixes styles

--- js/app.js
+++ js/app.js
@@ -2,11 +2,53 @@ var // DEFAULTS
 lat = 37.76487,
 lng = -122.41948,
 zoom = 3,
-minZoom = 3;
-maxZoom = 10;
+minZoom = 3,
+maxZoom = 16,
+previousZoom = 3,
+previousCenter;
 
-var projectsStyle      = { strokeColor: "#0D5FB2", strokeOpacity: 1, strokeWeight: 2, fillColor: "#FBDBBA", fillOpacity: 0.5 };
-var projectsHoverStyle = { strokeColor: "#0D5FB2", strokeOpacity: 1, strokeWeight: 2, fillColor: "#FBDBBA", fillOpacity: .7 };
+var mapSyles = [
+ {
+   featureType: "water",
+   stylers: [
+     { saturation: -99 },
+     { lightness: 25 }
+   ]
+ },{
+   featureType: "poi",
+   stylers: [
+     { saturation: -95 },
+     { lightness: 61 }
+   ]
+ },{
+   featureType: "administrative",
+   stylers: [
+     { saturation: -99 },
+     { gamma: 3.51 }
+   ]
+ },{
+   featureType: "road",
+   stylers: [
+     { visibility: "off" }
+   ]
+ },{
+   featureType: "road",
+   stylers: [
+     { visibility: "off" }
+   ]
+ },{
+   featureType: "landscape",
+   stylers: [
+     { saturation: -85 },
+     { lightness: 53 }
+   ]
+ },{
+ }
+];
+var nexsoStyle = new google.maps.StyledMapType(mapSyles, {name: "Nexso Style"});
+
+var projectsStyle      = { strokeColor: "#EB9827", strokeOpacity: .9, strokeWeight: 1, fillColor: "#FBDBBA", fillOpacity: .7 };
+var projectsHoverStyle = { strokeColor: "#693F07", strokeOpacity: 1, strokeWeight: 2, fillColor: "#FBDBBA", fillOpacity: .9 };
 
 $(function() {
 
@@ -20,6 +62,8 @@ $(function() {
   $("aside .close").on('click', function(e) {
     e.preventDefault();
     hideAside();
+    map.setZoom(previousZoom);
+    map.panTo(previousCenter);
   });
 
   function showAside() {
@@ -32,9 +76,8 @@ $(function() {
     });
   }
 
-
   // Slider
-  $( "#timeline .slider" ).slider({ range: true, min: 0, max: 500, step: 5, values: [ 75, 300 ], slide: function( event, ui ) { } });
+  // $( "#timeline .slider" ).slider({ range: true, min: 0, max: 500, step: 5, values: [ 75, 300 ], slide: function( event, ui ) { } });
 
   // Map
   var mapOptions = {
@@ -46,9 +89,12 @@ $(function() {
   };
 
   var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  map.mapTypes.set('nexsoStyle', nexsoStyle);
+  map.setMapTypeId('nexsoStyle');
 
   google.maps.event.addListener(map, 'zoom_changed', function() {
     infowindow.hide();
+
     $(".stations").css({width:$(document).width(), height:$(document).height(), top:0, left:0});
   });
 
@@ -104,77 +150,105 @@ $(function() {
     },
 
     initialize: function() {
-      this.ashoka   = this.options.ashoka;
-      this.agencies = this.options.agencies;
-      this.agencies.bind('reset', this.renderAgencies, this);
-      this.ashoka.bind('reset', this.renderAshoka, this);
       this.state = 1;
+      this.overlays = [];
+      this.addAgencies();
+      this.addAshokas();
+      this.addProjects();
     },
-    updateLayer: function() {
-      $("#map").css("width", $(document).width() + this.state + "px");
-      $("#map").css("height", $(document).height() + this.state + "px");
+    showOverlay: function(name, topic) {
+      if (this.overlays[name]) {
+        for (var i = 0; i < this.overlays[name].length; i++){
+          if (this.overlays[name][i].geojsonProperties.topic == topic.replace('t', ''))
+            this.overlays[name][i].setVisible(true);
+        }
+      }
     },
-    hideOverlay: function(c) {
-      $(".marker."+ c).hide();
-      this.updateLayer();
+    hideOverlay: function(name, topic) {
+      if (this.overlays[name]) {
+        for (var i = 0; i < this.overlays[name].length; i++){
+          if (this.overlays[name][i].geojsonProperties.topic == topic.replace('t', ''))
+            this.overlays[name][i].setVisible(false);
+        }
+      }
     },
-    showOverlay: function(c) {
-      $(".marker."+ c).show();
-      this.updateLayer();
-    },
-    removeOverlay: function(c) {
-      if (c == 'agencies') this.agencies.remove(this.agencies.models);
-      else if (c == 'ashoka') this.ashoka.remove(this.ashoka.models);
-      $(".marker."+ c).remove();
-      this.updateLayer();
-    },
-    removePath: function() {
-      if (this.projectsOverlay.length){
-        for (var i = 0; i < this.projectsOverlay.length; i++){
-          if(this.projectsOverlay[i].length){
-            for(var j = 0; j < this.projectsOverlay[i].length; j++){
-              this.projectsOverlay[i][j].setMap(null);
+    removeOverlay: function(name) {
+      if (name == "ashokas" || name == "agencies") {
+        for (var i = 0; i < this.overlays[name].length; i++){
+          this.overlays[name][i].setMap(null);
+        }
+      } else {
+
+        if (this.overlays[name].length){
+          for (var i = 0; i < this.overlays[name].length; i++){
+            if(this.overlays[name][i].length){
+              for(var j = 0; j < this.overlays[name][i].length; j++){
+                this.overlays[name][i][j].setMap(null);
+              }
             }
           }
         }
       }
     },
-    addPath: function() {
+    addAshokas: function() {
       var that = this;
-      var url = "https://nexso2.cartodb.com/api/v2/sql/?q=SELECT the_geom FROM working_areas&format=geojson";
-      var url = "https://nexso2.cartodb.com/api/v2/sql/?q=SELECT v1_projects.title, v1_projects.approval_date, v1_projects.external_project_url, v1_projects.location_verbatim, v1_projects.budget, working_areas.the_geom FROM v1_projects, working_areas, v1_project_work_areas WHERE v1_projects.cartodb_id = v1_project_work_areas.project_id AND working_areas.cartodb_id = v1_project_work_areas.id&format=geojson";
-      //var url = "https://nexso2.cartodb.com/api/v2/sql?q=SELECT the_geom FROM v1_agencies&format=geojson";
-      this.addOverlay(url);
+      var url = "https://nexso2.cartodb.com/api/v2/sql?q=SELECT the_geom, ashoka_url AS url, topic_id AS topic, name FROM v1_ashoka WHERE the_geom IS NOT NULL AND topic_id IS NOT NULL&format=geojson";
+      this.addOverlay("ashokas", url);
     },
-    addOverlay: function(url) {
+    addAgencies: function() {
+      var that = this;
+      var url = "https://nexso2.cartodb.com/api/v2/sql?q=SELECT the_geom, external_url AS url, name FROM v1_agencies WHERE the_geom IS NOT NULL&format=geojson";
+      this.addOverlay("agencies", url);
+    },
+    addProjects: function() {
+      var that = this;
+      var url = "https://nexso2.cartodb.com/api/v2/sql/?q=SELECT v1_projects.title, v1_projects.approval_date, v1_projects.external_project_url, v1_projects.location_verbatim, v1_projects.budget, working_areas.the_geom FROM v1_projects, working_areas, v1_project_work_areas WHERE v1_projects.cartodb_id = v1_project_work_areas.project_id AND working_areas.cartodb_id = v1_project_work_areas.id&format=geojson";
+      this.addOverlay("projects", url);
+    },
+    addOverlay: function(name, url) {
       var that = this;
       $.ajax({
         url: url,
         success: function(data) {
 
-          console.log(data);
-          function setInfoWindow() {
+
+          function setInfoWindow(overlay) {
+            google.maps.event.addListener(overlay, 'click', function(event) {
+
+              var 
+              title   = overlay.geojsonProperties.name,
+              moreURL = overlay.geojsonProperties.url;
+
+              infowindow.setContent(title, name);
+              infowindow.open(event.latLng);
+
+            });
           }
 
           function showFeature(geojson, style){
-            that.projectsOverlay = new GeoJSON(geojson, style || null);
+            try {
+              var data = JSON.parse(geojson);
+            } catch ( e ) {
+              var data = geojson;
+            }
+            that.overlays[name] = new GeoJSON(data, name, style || null);
 
-            if (that.projectsOverlay.type && that.projectsOverlay.type == "Error"){
-              alert(that.projectsOverlay.message);
+            if (that.overlays[name].type && that.overlays[name].type == "Error"){
+              console.log(that.overlays[name].message);
               return;
             }
 
-            if (that.projectsOverlay.length){
-              for (var i = 0; i < that.projectsOverlay.length; i++){
-                if(that.projectsOverlay[i].length){
-                  for(var j = 0; j < that.projectsOverlay[i].length; j++){
-                    that.projectsOverlay[i][j].setMap(map);
+            if (that.overlays[name].length){
+              for (var i = 0; i < that.overlays[name].length; i++){
+                if(that.overlays[name][i].length){
+                  for(var j = 0; j < that.overlays[name][i].length; j++){
+                    that.overlays[name][i][j].setMap(map);
 
                     // Overlay events
-                    google.maps.event.addListener(that.projectsOverlay[i][j], 'click', function(event) {
-                      console.log(this.geojsonProperties);
+                    google.maps.event.addListener(that.overlays[name][i][j], 'click', function(event) {
 
                       var 
+                      that         = this,
                       title        = this.geojsonProperties.title,
                       approvalDate = this.geojsonProperties.approval_date,
                       moreURL      = this.geojsonProperties.external_project_url,
@@ -184,6 +258,8 @@ $(function() {
                       infowindow.setContent(title, "project");
                       infowindow.setSolutionURL(title, moreURL);
                       infowindow.setCallback(function(e) {
+                        e.preventDefault();
+
                         hideAside(function() {
                           $("aside .content .header h2").html(title);
                           $("aside .content ul li.approvalDate span").text(approvalDate);
@@ -193,32 +269,40 @@ $(function() {
                           // $("aside .content ul li.solution span").text(approvalDate);
                           $("aside .content ul li.more a").attr("href", moreURL);
                           showAside();
+                          infowindow.hide();
+
+                          previousZoom = map.getZoom();
+                          previousCenter = map.getCenter();
+
+                          var bounds = new google.maps.LatLngBounds();
+                          that.getPath().forEach( function(latlng) { bounds.extend(latlng); } ); 
+                          map.fitBounds(bounds)
+
                         });
                       });
                       infowindow.open(event.latLng);
                     });
 
-                    google.maps.event.addListener(that.projectsOverlay[i][j], 'mouseover', function(event) {
+                    google.maps.event.addListener(that.overlays[name][i][j], 'mouseover', function(event) {
                       this.setOptions(projectsHoverStyle);
                     });
-                    google.maps.event.addListener(that.projectsOverlay[i][j], 'mouseout', function(event) {
-                      var projectsStyle = { strokeColor: "#EFC392", strokeOpacity: 1, strokeWeight: 2, fillColor: "#FBDBBA", fillOpacity: 0.5 };
+
+                    google.maps.event.addListener(that.overlays[name][i][j], 'mouseout', function(event) {
+                      var projectsStyle      = { strokeColor: "#EB9827", strokeOpacity: .9, strokeWeight: 1, fillColor: "#FBDBBA", fillOpacity: .7 };
                       this.setOptions(projectsStyle);
                     });
-
                   }
+                } else{
+                  that.overlays[name][i].setMap(map);
                 }
-                else{
-                  that.projectsOverlay[i].setMap(map);
-                }
-                if (that.projectsOverlay[i].geojsonProperties) {
-                  setInfoWindow(that.projectsOverlay[i]);
+                if (that.overlays[name][i].geojsonProperties) {
+                  setInfoWindow(that.overlays[name][i]);
                 }
               }
-            }else{
-              that.projectsOverlay.setMap(map)
-              if (that.projectsOverlay.geojsonProperties) {
-                setInfoWindow(that.projectsOverlay);
+            } else{
+              that.overlays[name].setMap(map)
+              if (that.overlays[name].geojsonProperties) {
+                setInfoWindow(that.overlays[name]);
               }
             }
 
@@ -226,69 +310,12 @@ $(function() {
           showFeature(data, projectsStyle);
         }
       });
-
-    },
-
-    addOverlay2: function(data, c) {
-      //this.removeOverlay(c);
-
-      var self = this;
-      var overlay = new google.maps.OverlayView();
-
-      overlay.onRemove = function() { }
-      // Add the container when the overlay is added to the map.
-      overlay.onAdd = function() {
-
-        if (!self.layer) {
-          self.layer = d3.select(this.getPanes().overlayMouseTarget)
-          .attr("class", "stations")
-        } 
-        self.updateLayer();
-      }
-
-      // Draw each marker as a separate SVG element.
-      overlay.draw = function() {
-        var projection = this.getProjection();
-
-        var markers = self.layer.selectAll("svg." + c)
-        .data(data)
-        .each(transform) // update existing markers
-        .enter().append("svg:svg")
-        .each(transform);
-
-        function transform(point) {
-          var latLng = new google.maps.LatLng(point.lat(), point.lng());
-          var position = projection.fromLatLngToDivPixel(latLng);
-
-          var markerClass;
-
-          if (point.topic_id) {
-            markerClass = "marker " + c + " t" + point.topic_id(); 
-          } else {
-            markerClass = "marker " + c; 
-          }
-
-          if (point.topic_id == null) return;
-
-          return d3.select(this)
-          .on('click', function(){ 
-            infowindow.setContent(point.name(), c);
-            infowindow.open(latLng);
-          })
-          .style("left", position.x + "px")
-          .style("top", position.y + "px")
-          .attr("class", markerClass);
-        }
-      };
-
-      // Bind our overlay to the map…
-      overlay.setMap(map);
     },
     renderAgencies: function() {
-      this.addOverlay2(this.agencies.models, 'agencies');
+      this.addMarkerOverlay(this.agencies.models, 'agencies');
     },
     renderAshoka: function() {
-      this.addOverlay2(this.ashoka.models, 'ashoka');
+      this.addMarkerOverlay(this.ashoka.models, 'ashoka');
     }
   });
 
@@ -313,19 +340,17 @@ $(function() {
         var c  = $(this).attr('class').replace(/selected/, "").trim();
 
         if ($(this).hasClass('selected')) {
-          if (id == "agencies") agencies.fetch();
-          else if (id == "ashoka") ashoka.fetch();
-          else if (id == "projects") mapView.addPath();
-          else {
-            mapView.showOverlay(c);
+          if (id == "agencies") mapView.addAgencies();
+          else if (id == "ashokas") mapView.addAshokas();
+          else if (id == "projects") mapView.addProjects();
+          else if (c) {
+            mapView.showOverlay("ashokas", c);
           }
         } else {
-          if (id == "agencies" || id == "ashoka") {
+          if (id == "projects" || id == "agencies" || id == "ashokas") {
             mapView.removeOverlay(id);
-          } 
-          else if (id == "projects") mapView.removePath();
-          else {
-            mapView.hideOverlay(c);
+          } else if (c){
+            mapView.hideOverlay("ashokas", c);
           }
         }
 
@@ -344,10 +369,4 @@ $(function() {
   var filterView = new FilterView({
     el:$('nav .content')
   });
-
 });
-
-
-
-
-
